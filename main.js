96fe655d8a2958195426aa05539ac2ec3b549497
Aarav-coder11 <75116259+Aarav-coder11@users.noreply.github.com>
2022-07-03 16:51:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function prelaod(){
+}
+
+function setup(){
+    canvas = createCanvas(400, 300);
+    canvas.position(450, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+
+    tint_color = "";
+}
+
+function draw(){
+    image(video, 0, 0, 400, 300);
+    tint(tint_color);
+}
+
+function take_snapshot(){
+    save('student_name.png')
+}
+
+function filter_tint(){
+    tint_color = document.getElementById("color_name").value;
+}
